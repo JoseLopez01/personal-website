b213100636be27442265a223fb75ce2a8049606c
Gian López <lopezarizagianlucas@gmail.com>
2021-07-14 22:21:28 -0500

    Hero finished

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,33 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
-    <nav className="max-h-10 h-10 bg-gray-300 rounded">
-      <div className="flex justify-between items-center h-full p-4">
+    <nav className="max-h-10 h-10 bg-gray-300 md:rounded">
+      <div className="flex justify-between items-center h-full px-4">
         <div>Home</div>
         <div className="hidden md:block">
           <div>
-            <a
-              className="transition duration-500 mx-3 px-3 py-2 rounded text-sm hover:bg-gray-400"
-              href="#"
+            <Link
+              href="/"
             >
-              Link #1
-            </a>
-            <a
-              className="transition duration-500 mx-3 px-3 py-2 rounded text-sm hover:bg-gray-400"
-              href="#"
+              <a className="transition duration-500 mx-3 px-3 py-2 rounded text-sm hover:bg-gray-400">
+                Home
+              </a>
+            </Link>
+            <Link
+              href="/work"
             >
-              Link #2
-            </a>
-            <a
-              className="transition duration-500 mx-3 px-3 py-2 rounded text-sm hover:bg-gray-400"
-              href="#"
+              <a className="transition duration-500 mx-3 px-3 py-2 rounded text-sm hover:bg-gray-400">
+                Work
+              </a>
+            </Link>
+            <Link
+              href="/"
             >
-              Link #3
-            </a>
+              <a className="transition duration-500 mx-3 px-3 py-2 rounded text-sm hover:bg-gray-400">
+                Contact
+              </a>
+            </Link>
           </div>
         </div>
       </div>
---
+++ pages/work/index.js
@@ -0,0 +1,14 @@
+export default function Work() {
+  return (
+    <div className="p-10">
+      <div className="w-full">
+        <p className="text-lg text-center font-bold text-gray-500">
+          Portfolio
+        </p>
+      </div>
+      <div className="w-full bg-gray-500 mt-10 h-10 rounded">
+
+      </div>
+    </div>
+  );
+}
