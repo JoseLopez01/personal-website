b0b16dfa131b067fb7ccaba8627c012469f1b0e9
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2021-07-24 16:36:30 -0500

    Small change

--- components/Project.js
+++ components/Project.js
@@ -4,13 +4,13 @@ export default function Project() {
   return (
     <div className="col-span-4 flex mx-auto shadow-lg rounded-md">
       <div className="p-6 flex-auto">
-        <pre className="text-lg font-normal">
+        <p className="text-lg font-normal font-mono">
           All of my experience have been made in a private organization from Colombia, Soft-G Net. <br />
           Here, I'm doing my role like a web and mobile developer using technologies like Angular, Ionic, <br />
           Capacitor, etc. <br />
           In every project Redux is used, which has made get a good knowledge of this pattern. <br />
           I'm not allowed to show this projects, but some can be found in Google Play Store, and Apple Store. <br />
-        </pre>
+        </p>
       </div>
     </div>
   );
