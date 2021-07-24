7f7271aba2e992cc2f1129bdd436d658796e091f
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2021-07-24 16:18:02 -0500

    Loading added on submit contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import emailjs from 'emailjs-com';
+import LoadingDots from './LoadingDots';
 
 const initialState = {
   email: '',
   name: '',
   subject: '',
   idea: '',
-}
+};
 
 export default function Contact() {
   const [form, setForm] = useState(initialState);
+  const [loading, setLoading] = useState(false);
 
   const { email, name, subject, idea } = form;
 
@@ -26,6 +28,7 @@ export default function Contact() {
 
   function handleSubmit(e) {
     e.preventDefault();
+    setLoading(true);
     emailjs
       .send(
         process.env.EMAILJS_SERVICE_ID,
@@ -41,16 +44,20 @@ export default function Contact() {
       .then((response) => {
         if (response.status === 200) {
           resetForm();
+          setLoading(false);
         }
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(() => {
+        setLoading(false);
       });
   }
 
   return (
     <>
-      <div className="text-4xl pt-10 font-bold md:text-5xl text-center md:m-10" id="contact">
+      <div
+        className="text-4xl pt-10 font-bold md:text-5xl text-center md:m-10"
+        id="contact"
+      >
         <span className="text-blue-800">Let's </span>
         get in
         <span className="text-blue-800"> touch</span>
@@ -60,43 +67,48 @@ export default function Contact() {
         autoComplete="off"
         onSubmit={handleSubmit}
       >
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2"
-          value={name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2"
-          value={email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Subject"
-          name="subject"
-          className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2"
-          value={subject}
-          onChange={handleChange}
-          required
-        />
-        <textarea
-          placeholder="Write your idea"
-          name="idea"
-          className="h-24 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2 resize-none"
-          value={idea}
-          onChange={handleChange}
-          required
-        ></textarea>
-        <button className="bg-gray-800 hover:bg-gray-900 text-white font-semibold py-4 w-full rounded-md">
-          Submit
+        <fieldset disabled={loading}>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2"
+            value={name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            name="subject"
+            className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2"
+            value={subject}
+            onChange={handleChange}
+            required
+          />
+          <textarea
+            placeholder="Write your idea"
+            name="idea"
+            className="h-24 focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-3 my-2 resize-none"
+            value={idea}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </fieldset>
+        <button
+          className="bg-gray-800 hover:bg-gray-900 text-white font-semibold py-4 w-full rounded-md"
+          disabled={loading}
+        >
+          {loading ? <LoadingDots /> : 'Submit'}
         </button>
       </form>
     </>
---
+++ components/LoadingDots.js
@@ -0,0 +1,9 @@
+export default function LoadingDots() {
+  return (
+    <div className="flex items-center justify-center loading">
+      <span className="h-4 w-4 mx-1 bg-white rounded-full inline-block"></span>
+      <span className="h-4 w-4 mx-1 bg-white rounded-full inline-block"></span>
+      <span className="h-4 w-4 mx-1 bg-white rounded-full inline-block"></span>
+    </div>
+  );
+}
