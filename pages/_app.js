92de3160ff56c11086e47916d24efb4d8dcbdd9f
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2021-09-28 16:52:19 -0500

    property

--- pages/_app.js
+++ pages/_app.js
@@ -14,11 +14,11 @@ function MyApp({ Component, pageProps }) {
         <title>Jose Lopez - Web Developer</title>
         <meta name="description" content="This is my portfolio, a resume of my work and the tools i use like, Angular, React, Flutter, Redux, for frontend, web, and mobile development" />
         <meta name="keywords" content="web, developer, react, angular, colombia, caribbean, node, flutter, ionic, capacitor, cordova, mobile" />
-        <meta name="og:description" content="This is my portfolio, a resume of my work and the tools i use like, Angular, React, Flutter, Redux, for frontend, web, and mobile development" />
-        <meta name="og:title" content="Jose Lopez - Web Developer" />
-        <meta name="og:url" content="https://j-lopez.herokuapp.com/" />
-        <meta name="og:type" content="website" />
-        <meta name="og:image" content="images/logo.png" />
+        <meta property="og:description" content="This is my portfolio, a resume of my work and the tools i use like, Angular, React, Flutter, Redux, for frontend, web, and mobile development" />
+        <meta property="og:title" content="Jose Lopez - Web Developer" />
+        <meta property="og:url" content="https://j-lopez.herokuapp.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="images/logo.png" />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
