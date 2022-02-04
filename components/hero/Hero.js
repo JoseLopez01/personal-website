740a2bedfa05a9d8e674e1ccc5827a847531e2df
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-02-04 06:42:41 -0500

    Fix: description

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import ScrollIndicator from '../ScrollIndicator';
 import Skills from '../Skills';
 import MyDescription from './MyDescription';
@@ -7,9 +9,11 @@ export default function Hero() {
     <>
       <div className="grid grid-cols-2 h-1/2" id="about-me">
         <div className="col-span-1 relative">
-          <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
             <MyDescription />
           </div>
+        </div>
+        <div className="col-span-1 relative">
         </div>
       </div>
     </>
--- components/hero/MyDescription.js
+++ components/hero/MyDescription.js
@@ -3,9 +3,9 @@ import React from 'react';
 function MyDescription() {
   return (
     <>
-      <p className="text-5xl font-bold mb-8">
-        Hi, I'm <span className="text-blue-700 text-7xl">José López</span>{' '}
-        <br />
+      <p className="text-5xl font-bold mb-4">
+        Hi, I'm <br />{' '}
+        <span className="text-blue-700 text-7xl">José López</span>
       </p>
       <p className="text-gray-600 text-sm mb-1">
         An experienced software developer from Colombia.
