7de89853b1ed5365c81e1342fabd5b0a90790a6f
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-02-06 16:17:41 -0500

    Fix

--- components/header/LocalLink.js
+++ components/header/LocalLink.js
@@ -1,6 +1,3 @@
-import Link from 'next/link';
-import PropTypes from 'prop-types';
-
 export default function LocalLink({ routeName, pathName, className, onClick }) {
   return (
     <a href={pathName} onClick={onClick}>
@@ -8,9 +5,3 @@ export default function LocalLink({ routeName, pathName, className, onClick }) {
     </a>
   );
 }
-
-LocalLink.propTypes = {
-  routeName: PropTypes.string.isRequired,
-  pathName: PropTypes.string.isRequired,
-  passHref: PropTypes.bool,
-};
