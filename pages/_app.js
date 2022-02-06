c195383d84d161509bd44687ab078b71c98d0c04
Jose Alejandro Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-02-06 16:32:11 -0500

    a

--- pages/_app.js
+++ pages/_app.js
@@ -24,7 +24,10 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content="Jose Lopez - Web Developer" />
         <meta property="og:url" content="www.lopez-ariza.com" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/images/logo.png" />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/personal-web-26017.appspot.com/o/logo.jpg?alt=media&token=0ce90842-b7ac-4334-b9f9-73d082548596"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
